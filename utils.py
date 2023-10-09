25e3674a73a71b1529a8382da07cfd2e12d56f49
amar-larkai <147026725+amar-larkai@users.noreply.github.com>
2023-10-09 16:09:57 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -26,8 +26,26 @@ from langchain.document_loaders import UnstructuredURLLoader
 from langchain.llms import AzureOpenAI
 import openai
 
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+import numpy as np
+import torch
+import cv2
+import torch.nn as nn
+from transformers import ViTModel, ViTConfig
+from torchvision import transforms
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+config=ViTConfig()
+
+config.hidden_size=1024
+config.intermediate_size=4096
+config.num_attention_heads=16
+config.num_hidden_layers=24
+config.patch_size=16
 
 # csv_path = ""
 # csv_data = CSVLoader(file_path=csv_path).load()
@@ -92,3 +110,65 @@ def get_model():
         chain_type="stuff",
     )
     return chain
+
+
+class ImageDataset(torch.utils.data.Dataset):
+
+  def __init__(self, input_data):
+
+      self.input_data = input_data
+      # Transform input data
+      self.transform = transforms.Compose([
+        transforms.Resize((224, 224), antialias=True),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
+        ])
+
+  def __len__(self):
+      return len(self.input_data)
+
+  def get_images(self, idx):
+      return self.transform(self.input_data[idx]['image'])
+
+  def get_labels(self, idx):
+      return self.input_data[idx]['label']
+
+  def __getitem__(self, idx):
+      # Get input data in a batch
+      train_images = self.get_images(idx)
+      train_labels = self.get_labels(idx)
+
+      return train_images, train_labels
+  
+
+label2id = {'myocardial infarctions': 0, 'normal': 1}
+id2label = {0: 'myocardial infarctions', 1: 'normal'}
+
+class ViT(nn.Module):
+
+  def __init__(self, config=config, num_labels=2,
+               model_checkpoint='google/vit-large-patch16-224-in21k'):
+
+        super(ViT, self).__init__()
+
+        self.vit = ViTModel.from_pretrained(model_checkpoint, add_pooling_layer=False)
+        self.classifier = (
+            nn.Linear(config.hidden_size, num_labels)
+        )
+
+  def forward(self, x):
+
+    x = self.vit(x)['last_hidden_state']
+    # Use the embedding of [CLS] token
+    output = self.classifier(x[:, 0, :])
+
+    return output
+  
+
+def load_model():
+    with open('model.pkl', 'rb') as f:
+        model_state_dict = pickle.load(f)  
+    model = ViT()
+    model.load_state_dict(model_state_dict)  
+    return model  
